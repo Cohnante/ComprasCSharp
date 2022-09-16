65f8834d51033ed51d1939a530f68d6a0058bc56
BrayanMedinaMoreno <brayan0519@yahoo.com>
2022-09-15 21:21:33 -0500

    segunda parte

--- primera parte.sql
+++ primera parte.sql
@@ -1,9 +1,12 @@
 -- Se elimina la base de datos distribuidora si existe --
 drop database if exists distribuidora;
+go
 -- se crea la base de datos --
 create database distribuidora;
+go
 -- se usa la base de datos --
 use distribuidora;
+go
 
 create table Empleado (id_empleado varchar (10) primary key not null,
 						nombre_empleado varchar (30) not null,
@@ -12,17 +15,20 @@ create table Empleado (id_empleado varchar (10) primary key not null,
 						edad_empleado varchar (14) not null,
 						direccion_empleado varchar (150) not null,
 						nacionalidad_empleado varchar (40) not null
+
 						);
+go
 create table Proveedor (id_proveedor varchar (30) primary key not null,
 						nombre_provedor varchar (30) not null,
 						telefono varchar (30) not null,
 						dirreccion_proveedor varchar (150) not null
 						);
+go
 create table Categoria (id_categoria varchar (30) primary key not null,
 						nombre varchar (30) not null,
 						caracteristica varchar (30) not null
 						);
-
+go
 create table cliente (id_cliente varchar (10) primary key not null,
 					  nombre_cliente varchar (30) not null,
 					  apellido_cliente varchar (30) not null,
@@ -31,17 +37,18 @@ create table cliente (id_cliente varchar (10) primary key not null,
 					  direccion_cliente varchar (150) not null,
 					  nacionalidad_cliente varchar (40) not null
 					  );
-
-create table ventas (numero_venta varchar (30) primary key not null,
+go
+create table ventas (numero_venta int identity primary key not null,
 					fecha date not null,
-					fk_empleado varchar (30) not null,
+					fk_empleado varchar (10) not null,
 					fk_cliente varchar (10) not null,
 					iva float null,
 					sub_total float null,
 					total float null,
-					foreign key (fk_cliente) references cliente(id_cliente)
+					foreign key (fk_cliente) references cliente(id_cliente),
+					foreign key (fk_empleado) references empleado(id_empleado)
 					);
-
+go
 create table Compra (id_compra varchar (30) primary key not null,
 					fk_empleado varchar (10) not null,
 					iva float null,
@@ -52,14 +59,14 @@ create table Compra (id_compra varchar (30) primary key not null,
 					foreign key (fk_empleado) references Empleado(id_empleado),
 					foreign key (fk_proveedor) references proveedor(id_proveedor)
 					);
-
+go
 create table producto (codigo_producto varchar (30) primary key not null,
 						nombre_producto varchar (30) not null,
 						valor float null,
 						fk_categoria varchar (30) not null,
 						foreign key (fk_categoria) references Categoria(id_categoria)
 						);
-
+go
 
 
 
@@ -73,16 +80,16 @@ create table Compra_Producto (
 							   constraint fk2 foreign key (codigo_pr) references producto (codigo_producto),
 							   primary key (num_compra,codigo_pr)
 							  );
-
+go
 create table ventas_producto (codigo_pr varchar (30) not null,
-							  num_venta varchar (30) not null,
+							  num_venta int not null,
 							  cantidad int not null,
 							  valor_total float not null,
 							  constraint fk3 foreign key (num_venta) references ventas (numero_venta),
 							  constraint fk4 foreign key (codigo_pr) references producto (codigo_producto),
 							  primary key (num_venta,codigo_pr)
 							  );
-
+go
 
 --create database oli
 use oli

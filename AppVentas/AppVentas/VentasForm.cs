e33320fa0ae51925052ed503b762e568f0b51d56
moisesph <mois.mp8@gmail.com>
2022-09-29 20:24:19 -0500

    Terminar front

---
+++ AppVentas/AppVentas/VentasForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibLNCompras;
+
+namespace AppVentas
+{
+    public partial class VentasForm : Form
+    {
+        public VentasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFormProductos_Click(object sender, EventArgs e)
+        {
+            FormProductos.Visible = true;
+            try
+            {
+                Producto objProducto = new Producto();
+                if (!objProducto.ComboCategoriaProductos(comboCategoriaProducto))
+                {
+                    MessageBox.Show(objProducto.Error);
+                    objProducto = null;
+                }
+                objProducto = null;
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
